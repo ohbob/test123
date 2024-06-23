10fcd4ef0606417e9a3fc95f3f00594fbfd75542
Camo <roberts@nallus.com>
2024-06-23 13:09:44 +0300

    initial

---
+++ src/lib/markdown.ts
@@ -0,0 +1,47 @@
+import sharp from 'sharp';
+
+import type { Hast, Image } from '$lib/types';
+
+export const reduceHast = (hast: Hast): Hast => {
+	// remove all unnessesary elements from hast (like position, properties or newlines) so no unnecessary information gets built
+	return {
+		type: hast.type,
+		...(hast.children && {
+			children: hast.children
+				.filter((child: Hast) => !child.value || child.value !== '\n')
+				.map((child: Hast) => reduceHast(child))
+		}),
+		...(hast.value && { value: hast.value }),
+		...(hast.tagName && { tagName: hast.tagName }),
+		...(hast.properties && { properties: hast.properties })
+	};
+};
+
+export const getImageProperties = async (path: string): Promise<Image> => {
+	let width = undefined;
+	let height = undefined;
+	try {
+		const metadata = await sharp(`src/${path}`).metadata();
+		width = !metadata.orientation || metadata.orientation <= 4 ? metadata.width : metadata.height;
+		height = !metadata.orientation || metadata.orientation <= 4 ? metadata.height : metadata.width;
+	} catch {
+		console.warn(`could not open src/${path}`);
+	}
+
+	return { src: encodeURI(path), width: width, height: height };
+};
+
+export const addImagePropertiesToHast = async (hast: Hast): Promise<Hast> => {
+	if (hast.tagName === 'img' && hast.properties && hast.properties.src) {
+		hast.properties = {
+			...(hast.properties ? hast.properties : {}),
+			...(await getImageProperties(hast.properties.src))
+		};
+	}
+	if (hast.children) {
+		hast.children = await Promise.all(
+			hast.children.map(async (child) => await addImagePropertiesToHast(child))
+		);
+	}
+	return hast;
+};
---
+++ svelte.config.js
@@ -0,0 +1,25 @@
+import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess({
+		scss: {
+			// We can use a path relative to the root because
+			// svelte-preprocess automatically adds it to `includePaths`
+			// if none is defined.
+			prependData: `@use 'src/lib/styles/variables.scss' as *;`
+		}
+	}),
+
+	kit: {
+		adapter: adapter({fallback: '404.html', strict: false}),
+		prerender: {
+			entries: ['/']
+		}
+	}
+};
+
+export default config;
